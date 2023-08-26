135cf35dfd11a34750e097c6743373c93665f0f8
Bryan Wood <itanex@gmail.com>
2023-08-26 08:52:23 -0700

    Add sequelize db config

---
+++ src/config/db.config.ts
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const config = {
+    HOST: process.env.DB_HOST,
+    USER: process.env.DB_USER,
+    PASSWORD: process.env.DB_PASSWORD,
+    DB: process.env.DB_NAME,
+    PORT: parseInt(process.env.DB_PORT || "5432"),
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
+
+export const dialect = "postgres";
